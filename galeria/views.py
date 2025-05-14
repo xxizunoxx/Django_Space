6c600b41773e0a417bf062cb8cdf66d7ee9dd8b5
xxizunoxx <caique_mk2@hotmail.com>
2025-05-14 14:19:37 -0300

    Django html/template

--- galeria/views.py
+++ galeria/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 def index(request):
-    return HttpResponse('<h1>Alura Django</h1> <p>Galeria de Imagens</p>')
+    return render(request, 'index.html')
