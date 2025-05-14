0652ed318b423eea50deb259273802056dd028ec
Caique-Camargo <caique_mk2@hotmail.com>
2025-05-14 19:39:07 -0300

    alterações de layout e configuração do CRUD

--- galeria/admin.py
+++ galeria/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from galeria.models import Fotografia
 
-# Register your models here.
+class ListandoFotografias(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'legenda', 'categoria')
+    list_display_links = ('id', 'nome')
+    search_fields = ('nome',)
+    list_filter = ('categoria',)
+
+admin.site.register(Fotografia, ListandoFotografias)
+
